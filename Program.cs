2bb1f2c61fb4a232db604c4cb920fde7af990661
Matheus <matheusdsc57@gmail.com>
2023-01-05 15:10:26 -0400

    Arquivos Padrão

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine(v1);
+        }
+    }
+}
