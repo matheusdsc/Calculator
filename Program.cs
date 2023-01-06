9f56992796726cecc902a0cf6f9665fcdda92a90
Matheus <matheusdsc57@gmail.com>
2023-01-06 10:34:57 -0400

    Finalizado!

--- Program.cs
+++ Program.cs
@@ -16,9 +16,13 @@ namespace Calculator
             Console.WriteLine("2. Subtração");
             Console.WriteLine("3. Divisão");
             Console.WriteLine("4. Multiplicação");
-            Console.WriteLine("5. Sair");
+            Console.WriteLine("5. Raiz quadrada");
+            Console.WriteLine("6. Potência");
+            Console.WriteLine("7. Porcentagem");
+            Console.WriteLine("8. Sair");
 
-            Console.WriteLine("-------------");
+
+            Console.WriteLine("--------------------");
             Console.Write("Selecione uma opção: ");
 
             int escolha = int.Parse(Console.ReadLine());
@@ -28,7 +32,10 @@ namespace Calculator
                 case 2: Subtracao(); break;
                 case 3: Divisao(); break;
                 case 4: Multiplicacao(); break;
-                case 5: System.Environment.Exit(0); break;
+                case 5: raiz(); break;
+                case 6: potencia(); break;
+                case 7: porcentagem(); break;
+                case 8: System.Environment.Exit(0); break;
                 default: Menu(); break;
             }
         }
@@ -88,6 +95,47 @@ namespace Calculator
             Console.WriteLine("");
             float resultado = v1 * v2;
             Console.WriteLine($"O reslultado da multiplicação é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void raiz()
+        {
+            Console.Clear();
+            Console.Write("Insira o valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            double resultado = Convert.ToSingle(Math.Sqrt(v1));
+            Console.WriteLine($"O reslultado da raiz quadrada é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void potencia()
+        {
+            Console.Clear();
+            Console.Write("Insira a base: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.Write("Insira o expoente: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            double resultado = Convert.ToSingle(Math.Pow(v1, v2));
+            Console.WriteLine($"O reslultado da potência é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void porcentagem()
+        {
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Valor da porcentagem: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 * (v2 / 100);
+            Console.WriteLine($"O reslultado da porcentagem é: {resultado}");
             Console.ReadKey();
             Menu();
         }
